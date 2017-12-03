10e327a29606b9afad6815494415cc727db066a6
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-03 11:06:29 -0800

    using fs and os to append to greetings.txt hello username

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 
+var user = os.userInfo();
 
-fs.appendFile('greetings.txt', 'Hello world!', function (err) {
+//console.log(user);
+
+fs.appendFile('greetings.txt',  `Hello ${user.username}!\n`, function (err) {
     if (err) {
         console.log('Unable to write to file');
     }
