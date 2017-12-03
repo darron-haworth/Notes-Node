4331fb9040bd0ed8dbf308457e14389992ce5ce4
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-03 12:07:01 -0800

    remove obsolete code

--- Notes.js
+++ Notes.js
@@ -1,7 +1,2 @@
 console.log('Starting notes.js');
 const os = require('os');
-module.exports.add = (a, b) => {
-
-    return `${a} + ${b} = ${a + b}${os.EOL}`;
-
-};
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./Notes');
 const _ = require('lodash');
 
-var user = os.userInfo();
+const notes = require('./Notes');
 
 // console.log(_.isString(true));
 // console.log(_.isString('Darron'));
@@ -14,19 +12,3 @@ var filteredArray = (_.uniq(['Frank', 'Darron', 3, 1, 'Andrew', 1, 'Darron', 2, 
 
 console.log(filteredArray.sort());
 
-// var res = notes.add(.3, -.5);
-// console.log(res);
-
-// res = notes.add(3265623.9090, -45654.5545415455);
-// console.log(res);
-
-//console.log(user);
-
-// fs.appendFile('greetings.txt',  `Hello ${user.username}! You are ${notes.age}${os.EOL}`, function (err) {
-//     if (err) {
-//         console.log('Unable to write to file');
-//     }
-//     else {
-//         console.log('wrote to greetings.txt');
-//     }
-// });
