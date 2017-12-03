8056661e1ebe390d1617ce8e13c2866059174703
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-03 12:05:33 -0800

    sort array

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var user = os.userInfo();
 // console.log(_.isString(true));
 // console.log(_.isString('Darron'));
 
-var filteredArray = (_.uniq(['Andrew', 1, 'Andrew', 1, 2, 3,4, 5]));
+var filteredArray = (_.uniq(['Frank', 'Darron', 3, 1, 'Andrew', 1, 'Darron', 2, 3,4, 5]));
 
-console.log(filteredArray);
+console.log(filteredArray.sort());
 
 // var res = notes.add(.3, -.5);
 // console.log(res);
