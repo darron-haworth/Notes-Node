e290e63d7f5f27f833c167116f86f1f1484221f0
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-03 11:52:01 -0800

    end of course 2

--- Notes.js
+++ Notes.js
@@ -1,1 +1,7 @@
 console.log('Starting notes.js');
+const os = require('os');
+module.exports.add = (a, b) => {
+
+    return `${a} + ${b} = ${a + b}${os.EOL}`;
+
+};
--- app.js
+++ app.js
@@ -3,16 +3,30 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./Notes');
+const _ = require('lodash');
 
 var user = os.userInfo();
 
+// console.log(_.isString(true));
+// console.log(_.isString('Darron'));
+
+var filteredArray = (_.uniq(['Andrew', 1, 'Andrew', 1, 2, 3,4, 5]));
+
+console.log(filteredArray);
+
+// var res = notes.add(.3, -.5);
+// console.log(res);
+
+// res = notes.add(3265623.9090, -45654.5545415455);
+// console.log(res);
+
 //console.log(user);
 
-fs.appendFile('greetings.txt',  `Hello ${user.username}!${os.EOL}`, function (err) {
-    if (err) {
-        console.log('Unable to write to file');
-    }
-    else {
-        console.log('wrote to greetings.txt');
-    }
-});
+// fs.appendFile('greetings.txt',  `Hello ${user.username}! You are ${notes.age}${os.EOL}`, function (err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+//     else {
+//         console.log('wrote to greetings.txt');
+//     }
+// });
