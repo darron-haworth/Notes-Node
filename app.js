0d35390a5b58fb56d9f239e1eb70c41ee0f59fd6
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-03 10:58:43 -0800

    use fs, append to greetings.txt

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log('Starting app.');
 
 const fs = require('fs');
 
+fs.appendFile('greetings.txt', 'Hello world!', function (err) {
+    if (err) {
+        console.log('Unable to write to file');
+    }
+    else {
+        console.log('wrote to greetings.txt');
+    }
+});
