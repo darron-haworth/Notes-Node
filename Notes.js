b86310935cdddd7b266e89231fb44b119e785ee9
Darron Haworth <darron.haworth@firsttechfed.com>
2017-12-26 16:40:34 -0800

    filled out addNote function

--- Notes.js
+++ Notes.js
@@ -1,8 +1,30 @@
 console.log('Starting notes.js');
 const os = require('os');
+const fs = require('fs');
 
 var addNote = (title, body) => {
     console.log('Adding note', `Title: ${title}, Body: ${body}`);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }
+    catch (err){
+
+    }
+
+    var duplicateNotes =notes.filter((note) => note.title === title);
+   
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+   
 };
 
 var getAll = () => {
